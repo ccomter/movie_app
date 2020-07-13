94aadbfd473d65f5287e166ad7cf3600394a9917
고건영 <gygo@funple.com>
2020-07-13 11:07:37 +0900

    Updates

---
+++ precache-manifest.d42658f70b8615a4224dba92cb86320e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b8fe1138a6d9ac63590ae264325203",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c2702a5fd4e15f96c473",
+    "url": "/movie_app/static/css/main.de1e21e2.chunk.css"
+  },
+  {
+    "revision": "2a948572c8a6408692a6",
+    "url": "/movie_app/static/js/2.48a578e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.48a578e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2702a5fd4e15f96c473",
+    "url": "/movie_app/static/js/main.203f3b88.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
